88f1f2ae69bbc0615ff25be3b78f71853c782209
Stiofan O'Connor <Stiofan@users.noreply.github.com>
2017-02-22 16:31:38 +0000

    Create test-product.php

---
+++ test-product.php
@@ -0,0 +1,14 @@
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Test Product
+ * Plugin URI:        http://ayecode.io/
+ * Description:       Update plugins provided by EDD software licencing or via github.
+ * Version:           1.0.5
+ * Author:            Stiofan O'Connor
+ * Author URI:        http://ayecode.io/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       external-updates
+ * Domain Path:       /languages
+ * Update URL:        https://github.com/AyeCode/test-product/
+ */
